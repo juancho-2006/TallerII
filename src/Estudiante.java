195d15319196213f9affcf0957976dac2641e1ef
juancho-2006 <juans.echeverrif@autonoma.edu.co>
2025-10-20 09:56:25 -0500

    actualizacion de unos try catch y mejoras de validacion de casos de borde

---
+++ src/Estudiante.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class Estudiante {
+    public String nombre;
+    public int id;
+    public Double promedio;
+
+    // Constructor inicializo los atributos
+    public Estudiante(String nombre, int id, Double promedio) {
+        this.nombre = nombre;
+        this.id = id;
+        this.promedio = promedio;
+    }
+
+    public void mostrarInf_Estu() {
+        System.out.println("******************************");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("ID: " + id);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("******************************");
+    }
+
+    public void calcularPromedio(List<Double> notas) {
+        double suma = 0.0;
+
+        for (int i = 0; i < notas.size(); i++) {
+            suma += notas.get(i);
+        }
+
+        if (!notas.isEmpty()) {
+            promedio = suma / notas.size();
+        } else {
+            promedio = 0.0;
+        }
+    }
+
+
+}
+}
