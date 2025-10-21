1709d1ddd4d294806bcd8f9f9b7c113578b1f483
juancho-2006 <juans.echeverrif@autonoma.edu.co>
2025-10-20 19:19:32 -0500

    se agrego el analisis de excepciones

--- src/AnálisisdeExcepciones.java
+++ src/AnálisisdeExcepciones.java
@@ -1,25 +1,33 @@
+/*
+programa con la logica corregida, Analisis de Excepciones corregido( nullpointer ->Arithmetic)
+atributos inicializados y creacion de constructor para mayor orden y repasar
+ */
+
 class main {
+    //Declaracion de Atributos
     public static class Calculadora {
         private int a;
         private int b;
 
+        //inicializacion  de atributos(constructor)
         public Calculadora(int numero1, int numero2) {
             this.a = numero1;
             this.b = numero2;
 
         }
 
+        // clase dividir con su respectivo manejo de errores
         public void dividir() {
             try {
                 int r = a / b;
                 System.out.println(r);
             } catch (ArithmeticException e) {
-                System.out.println("No se puede divir por cero  " + e);
-                ;
+                System.out.println("No se puede divir por cero  " + e);// e es para que muestre cual es el error;
             }
         }
     }
 
+    // main
     public static void main(String[] args) {
         Calculadora c = new Calculadora(10, 0);
         c.dividir();
